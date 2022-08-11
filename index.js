256c0a8c098b3dc016d1d3a9b0af8987dafc92d3
ChaseWelsh <fseusb@gmail.com>
2022-08-11 15:38:51 -0600

    this should work properly, the error bit is effectively identical to the solution branch.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function text_element(string) {
+    var paragraph = document.createElement("p");
+    paragraph.appendChild(document.createTextNode(string));
+    return paragraph;
+}
+
+function submitData(user_name, user_email) {
+    const data = JSON.stringify({
+        name: user_name,
+        email: user_email
+    })
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: data
+    }
+
+    fetch("http://localhost:3000/users", configurationObject).then(
+        function (response) {
+            return response.json();
+        }).then(function(something) {
+            document.body.innerHTML = something.id
+        })/*.catch(function(error_aaa) {
+            document.body.innerHTML = error_aaa.message
+        })*/
+}
